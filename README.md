f3b772f6b54100b99123b703675c6674c8a00f74
youngerheart <435495400@qq.com>
2015-12-22 11:53:12 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vue-daterangepicker
-simple daterangepicker for vue
+simple daterangepicker for vue, base on base on [daterangepicker](https://github.com/ElemeFE/daterangepicker)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const isProduction = process.env.NODE_ENV === 'production';
+const isWatch = process.env.NODE_ENV === 'watch';
+
+module.exports = {
+  entry: {
+    'vue-daterangepicker': './src/index.js'
+  },
+  output: {
+    path: './dist',
+    publicPath: '/dist/',
+    library: 'VueDateRangePicker',
+    libraryTarget: 'umd',
+    filename: isProduction ? '[name].min.js' : '[name].js'
+  },
+  babel: {
+    presets: ['es2015']
+  },
+  module: {
+    preLoaders: [
+      {test: /\.js$/, exclude: '/node_modules', loader: 'eslint'}
+    ],
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+      {test: /\.css$/, exclude: /node_modules/, loader: 'style!css!sass!'}
+    ]
+  }
+};
+
+if (isProduction) {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ];
+} else {
+  // 启动服务
+  if(isWatch) require('./server');
+}
